67d7a7f28f0378bf55a5e8bd57d64d1ff64e841a
ethan-goldstein <ethancgoldstein@gmail.com>
2025-04-16 18:08:50 -0400

    run

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
-window.onload = function () {
+window.onload = function() {
     let elements = document.getElementsByClassName('typewrite');
     for (let i = 0; i < elements.length; i++) {
-      let toRotate = elements[i].getAttribute('data-type');
-      let period = elements[i].getAttribute('data-period');
-      if (toRotate) {
-        new TxtType(elements[i], JSON.parse(toRotate), period);
-      }
+        let toRotate = elements[i].getAttribute('data-type');
+        let period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
     }
     let css = document.createElement("style");
     css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #1a1a1a }";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #f1f1f1 }";
     document.body.appendChild(css);
-  };
-  
-  let TxtType = function (el, toRotate, period) {
+};
+
+let TxtType = function(el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
     this.loopNum = 0;
@@ -21,34 +21,27 @@ window.onload = function () {
     this.txt = '';
     this.tick();
     this.isDeleting = false;
-  };
-  
-  TxtType.prototype.tick = function () {
+};
+
+TxtType.prototype.tick = function() {
     let i = this.loopNum % this.toRotate.length;
     let fullTxt = this.toRotate[i];
-  
+
     this.txt = this.isDeleting ? fullTxt.substring(0, this.txt.length - 1) : fullTxt.substring(0, this.txt.length + 1);
     this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-  
+
     let that = this;
     let delta = 200 - Math.random() * 100;
-  
+
     if (this.isDeleting) { delta /= 2; }
     if (!this.isDeleting && this.txt === fullTxt) {
-      delta = this.period;
-      this.isDeleting = true;
+        delta = this.period;
+        this.isDeleting = true;
     } else if (this.isDeleting && this.txt === '') {
-      this.isDeleting = false;
-      this.loopNum++;
-      delta = 500;
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
     }
-  
-    setTimeout(function () { that.tick(); }, delta);
-  };
-  window.onload = function () {
-    // ... existing code ...
-    let css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #f1f1f1 }"; // Updated cursor color
-    document.body.appendChild(css);
+
+    setTimeout(function() { that.tick(); }, delta);
 };

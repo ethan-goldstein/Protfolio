e4c9b31ee7ec93f0783d26d90bc4c04ee852b510
ethan-goldstein <ethancgoldstein@gmail.com>
2025-04-15 22:08:57 -0400

    commit

--- js/app.js
+++ js/app.js
@@ -1,104 +1,91 @@
-particlesJS.load('particles-js', '../particles.json', function() {});
-
-window.onscroll = function(){
-  if (window.scrollY >= 420){
-    document.getElementById("arrow").setAttribute("style", "display: none");
-  }
-  else{
-    document.getElementById("arrow").setAttribute("style", "display: block");
-  }
-}
-
-let TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-};
-
-TxtType.prototype.tick = function() {
-    let i = this.loopNum % this.toRotate.length;
-    let fullTxt = this.toRotate[i];
-
-    if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-    let that = this;
-    let delta = 200 - Math.random() * 100;
-
-    if (this.isDeleting) { delta /= 2; }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-    }
-
-    setTimeout(function() {
-    that.tick();
-    }, delta);
-};
-
-window.onload = function() {
-    let elements = document.getElementsByClassName('typewrite');
-    for (let i=0; i<elements.length; i++) {
-        let toRotate = elements[i].getAttribute('data-type');
-        let period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-          new TxtType(elements[i], JSON.parse(toRotate), period);
+{
+    "particles"; {
+      "number"; {
+        "value"; 150,
+        "density"; {
+          "enable"; true,
+          "value_area"; 800
         }
-    }
-    // INJECT CSS
-    let css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #1a1a1a}";
-    document.body.appendChild(css);
-  };
-
-  let modal = document.getElementById("myModal");
-
-  // Get the button that opens the modal
-  let btn = document.getElementById("myBtn");
-  
-  // Get the <span> element that closes the modal
-  let span = document.getElementsByClassName("close")[0];
-  
-  // When the user clicks on the button, open the modal 
-  btn.onclick = function() {
-    modal.style.display = "block";
+      };
+      "color"; {
+        "value"; "#c1c5c9"
+      };
+      "shape"; {
+        "type"; "circle"
+      };
+      "opacity"; {
+        "value"; 0.8,
+        "random"; true,
+        "anim"; {
+          "enable"; true,
+          "speed"; 1,
+          "opacity_min"; 0,
+          "sync"; false
+        }
+      };
+      "size"; {
+        "value"; 3,
+        "random"; true
+      };
+      "line_linked"; {
+        "enable"; true,
+        "distance"; 40,
+        "color"; "#c1c5c9",
+        "opacity"; 0.18,
+        "width"; 1
+      };
+      "move"; {
+        "enable"; true,
+        "speed"; 2,
+        "direction"; "none",
+        "random"; true,
+        "straight";false,
+        "out_mode"; "out",
+        "bounce"; true,
+        "attract"; {
+          "enable"; true,
+          "rotateX"; 600,
+          "rotateY"; 600
+        }
+      }
+    };
+    "interactivity"; {
+      "detect_on"; "window",
+      "events"; {
+        "onhover"; {
+          "enable"; true,
+          "mode"; "repulse"
+        };
+        "onclick"; {
+          "enable"; false
+        };
+        "resize"; true
+      };
+      "modes"; {
+        "grab"; {
+          "distance"; 400,
+          "line_linked"; {
+            "opacity"; 0.3
+          }
+        };
+        "bubble"; {
+          "distance"; 250,
+          "size"; 0,
+          "duration"; 2,
+          "opacity"; 0,
+          "speed"; 3
+        };
+        "repulse"; {
+          "distance"; 100,
+          "duration"; 0.4
+        };
+        "push"; {
+          "particles_nb"; 4
+        };
+        "remove"; {
+          "particles_nb"; 2
+        }
+      }
+    };
+    "retina_detect"; true
   }
-  
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
-  
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
-
-function showImage(){
-  if (
-    $("#sparc-mac-pic").is(":visible")){
-    $("#sparc-mac-pic").hide();
-    $("#sparc-mac-button").html("Show");
-  }
-  else{
-    $("#sparc-mac-pic").show();
-    $("#sparc-mac-button").html("Hide");
-  }
-}

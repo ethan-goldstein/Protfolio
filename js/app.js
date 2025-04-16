b56d9580f5da1193a6ed27b791ff229e4a4d3c61
ethan-goldstein <ethancgoldstein@gmail.com>
2025-04-15 20:46:14 -0400

    Bang

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
-particlesJS.load("particles-js", "particles.json", function() {
-    console.log("Particles.js config loaded.");
+particlesJS.load('particles-js', 'particles.json', function () {
+    console.log('particles.js config loaded');
   });
   
   window.onload = function () {
-    let elements = document.getElementsByClassName("typewrite");
+    let elements = document.getElementsByClassName('typewrite');
     for (let i = 0; i < elements.length; i++) {
-      let toRotate = elements[i].getAttribute("data-type");
-      let period = elements[i].getAttribute("data-period");
+      let toRotate = elements[i].getAttribute('data-type');
+      let period = elements[i].getAttribute('data-period');
       if (toRotate) {
         new TxtType(elements[i], JSON.parse(toRotate), period);
       }
     }
   
-    let css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #c1c5c9}";
+    let css = document.createElement('style');
+    css.type = 'text/css';
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #1a1a1a}";
     document.body.appendChild(css);
   };
   
@@ -23,7 +23,7 @@ particlesJS.load("particles-js", "particles.json", function() {
     this.el = el;
     this.loopNum = 0;
     this.period = parseInt(period, 10) || 2000;
-    this.txt = "";
+    this.txt = '';
     this.tick();
     this.isDeleting = false;
   };
@@ -31,33 +31,20 @@ particlesJS.load("particles-js", "particles.json", function() {
   TxtType.prototype.tick = function () {
     let i = this.loopNum % this.toRotate.length;
     let fullTxt = this.toRotate[i];
-  
-    if (this.isDeleting) {
-      this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-      this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-  
-    this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
-  
+    this.txt = this.isDeleting ? fullTxt.substring(0, this.txt.length - 1) : fullTxt.substring(0, this.txt.length + 1);
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
     let that = this;
     let delta = 200 - Math.random() * 100;
-  
-    if (this.isDeleting) {
-      delta /= 2;
-    }
-  
+    if (this.isDeleting) delta /= 2;
     if (!this.isDeleting && this.txt === fullTxt) {
       delta = this.period;
       this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === "") {
+    } else if (this.isDeleting && this.txt === '') {
       this.isDeleting = false;
       this.loopNum++;
       delta = 500;
     }
-  
     setTimeout(function () {
       that.tick();
     }, delta);
   };
-  
